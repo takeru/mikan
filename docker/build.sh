0f733bccb06e59be46e49302a5a1f49a3886f0c1
sasaki takeru <sasaki.takeru@gmail.com>
2021-03-31 13:42:38 +0900

    init

---
+++ docker/build.sh
@@ -0,0 +1,4 @@
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
+docker build -t mikan .
---
+++ docker/run.sh
@@ -0,0 +1,15 @@
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+touch ./bash_history
+
+docker run \
+  -v $SCRIPT_DIR/share:/share \
+  -v $SCRIPT_DIR/bash_history:/home/mikan/.bash_history \
+  --rm \
+  --net host \
+  -e DISPLAY=`echo $DISPLAY` \
+  -v $HOME/.Xauthority:/home/mikan/.Xauthority \
+  --privileged \
+  --name mikan-dev \
+  -it \
+  mikan /bin/bash
